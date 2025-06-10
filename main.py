61b99a161f009e76de56bc5ebb657642b4a937b4
sn2025120 <sn2025120@shs.hs.kr>
2025-06-10 14:40:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("영준app")
 st.write("hello")
+name=st.text_input("코드입력")
+if name:
+  if name=="yj":
+    st.success("반갑습니다. 유영준님!")
+  else:
+    st.warning("error401")
